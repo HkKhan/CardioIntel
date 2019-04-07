9572a2fc564b0ee091fc1cc6bff0c3b9fa1ed581
Pranav Addepalli <pranav.addepalli@gmail.com>
2019-04-07 05:30:27 -0400

    .Fixed all html code

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ import keras.layers
 from keras.models import model_from_json
 import numpy as np
 import os
+from os import listdir
+from os.path import isfile, join
+
 import scipy.io.wavfile
 
 
@@ -28,16 +31,30 @@ def buildModel():
     model.load_weights("model.h5")
     print("Loaded model from disk")
     return model
+
 def extractSin():
-    filename = "uploads/artifact__201105061143.wav"
+    userUpload = [f for f in listdir("uploads") if isfile(join("uploads", f))]
+    filename = "uploads/" + userUpload[0]
     print("file", filename)
     fs, data = scipy.io.wavfile.read(filename)
     print("data", len(data))
-    data = data.reshape(-1,1)
+    data = data.reshape(-1,1).T
     data = keras.preprocessing.keras_preprocessing.sequence.pad_sequences(data, maxlen=396900, dtype = np.float64, padding = 'post', value = 0)
     data = np.sin(data)
+    data = data.reshape(1,396900,1)
     return data
-
+def makeStringPrediction(prediction):
+    max = np.argmax(prediction)
+    stringPrediction = ""
+    if max==0:
+        stringPrediction = "Artifact"
+    if max==1:
+        stringPrediction = "Extrasystole"
+    if max==2:
+        stringPrediction = "Murmur"
+    if max==3:
+        stringPrediction = "Normal"
+    return stringPrediction
 #0 is Artifact
 #1 is Extrasystole
 #2 is Murmur
@@ -45,7 +62,6 @@ def extractSin():
 
 @app.route('/')
 def home():
-    print(extractSin())
     return render_template('index0.html')
 
 @app.route('/uploader', methods = ['GET', 'POST'])
@@ -55,10 +71,9 @@ def upload_file():
       if f and allowed_file(f.filename):
           f.save(os.path.join(app.config['UPLOAD_FOLDER'],secure_filename(f.filename)))
           model = buildModel()
-          #prediction = model.predict(extractSin())
-          print(prediction)
-          stringPrediction = "Normal"
-          return render_template('output.html', value=stringPrediction)
+          prediction = model.predict(extractSin())
+          stringPrediction = makeStringPrediction(prediction)
+          return render_template('output.html', value=stringPrediction, artifactChance=round(100*prediction[0][0], 2), extraChance=round(100*prediction[0][1],2), murmurChance=round(100*prediction[0][2], 2), normalChance=round(100*prediction[0][3], 2))
       else:
           return render_template('wrongfile.html')
 
